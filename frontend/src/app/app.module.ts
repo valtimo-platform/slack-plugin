2ac6caf1730867cf166cba7b0140e016a1ae54ec
Klaas Schuijtemaker <klaas.schuijtemaker@ritense.com>
2026-04-21 12:30:24 +0200

    Fix app.module.ts: use SlackPluginModule and slackPluginSpecification

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -75,7 +75,7 @@ import {TeamsModule} from "@valtimo/teams";
 import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
 import {environment} from "../environments/environment";
 
-import {SamplePluginModule, samplePluginSpecification,} from "@valtimo-plugins/slack";
+import {SlackPluginModule, slackPluginSpecification,} from "@valtimo-plugins/slack";
 
 export function tabsFactory() {
   return new Map<string, object>([
@@ -124,7 +124,7 @@ export function tabsFactory() {
     ProcessModule,
     ReactiveFormsModule,
     ResourceModule,
-    SamplePluginModule,
+    SlackPluginModule,
     SecurityModule,
     SseModule,
     SwaggerModule,
@@ -145,7 +145,7 @@ export function tabsFactory() {
     {
       provide: PLUGINS_TOKEN,
       useValue: [
-        samplePluginSpecification,
+        slackPluginSpecification,
       ],
     },
   ],
